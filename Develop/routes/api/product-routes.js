bb41f59c8cea6ec7d4c38a1896755a89a7df98cd
abraham perez reyes <abrahampre27@gmail.com>
2021-09-19 19:41:22 -0500

    currently create a product works and get all product works

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -5,14 +5,53 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // get all products
 router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+  Product.findAll({
+    attributes:['id','product_name','price','stock','category_id'],
+
+    include:[
+      {
+      model: Category,
+      attributes:['category_name']
+    },
+    {
+      model:Tag,
+      attributes:['tag_name'],
+      through: ProductTag,
+      as:'tags'
+    }
+    ]
+  })
+  .then(dbProductData =>res.json(dbProductData))
+  .catch(err=>{
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where:{
+      id:req.params.id
+    },
+    include:[
+      {
+        model: Category,
+        attributes:['id','category_name']
+      },
+      {
+        model: Tag,
+        attributes:['id','tag_name']
+      }
+    ]
+  })
+  .then(dbProductData =>res.json(dbProductData))
+  .catch(err =>{
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 // create new product
